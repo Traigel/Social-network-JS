45605be9359ea24f3c92356fe0a1445f572348df
Vova Traihel <vovatraigel@gmail.com>
2022-04-13 17:54:04 +0300

    14 css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from'./Header.module.css';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={classes.header}>
       <img src="https://e7.pngegg.com/pngimages/65/926/png-clipart-inkscape-graphics-editor-graphics-software-corel-draw-logo-silhouette-black-thumbnail.png" />
     </header>
   );
