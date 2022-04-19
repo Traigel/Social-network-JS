86eaee5b750f445326fe62a7d7bba0f88bbd9484
Vova Traihel <vovatraigel@gmail.com>
2022-04-19 22:51:20 +0300

    28 index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,28 +3,10 @@ import DialogItem from "./DialogItem/DialogItem";
 import classes from "./Dialogs.module.css";
 import Message from "./Message/Message";
 
-let dialogs = [
-  { id: 1, name: "Dimych" },
-  { id: 2, name: "Andrey" },
-  { id: 3, name: "Sveta" },
-  { id: 4, name: "Sasha" },
-  { id: 5, name: "Viktor" },
-  { id: 6, name: "Valera" },
-];
-
-let dialogsElements = dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} /> );
-
-let messages = [
-    { id: 1, message: "Hi" },
-    { id: 2, message: "Now is your it-kamasutra?" },
-    { id: 3, message: "Yo" },
-    { id: 3, message: "Yo" },
-    { id: 3, message: "Yo" },
-  ];
-
-  let messageElements = messages.map((message) => <Message message={message.message} />  );
-
 const Dialogs = (props) => {
+let dialogsElements = props.dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} /> );
+let messageElements = props.messages.map((message) => <Message message={message.message} />  );
+
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-let posts = [
-  { id: 1, message: "Hi, how are you?", likesCount: 39 },
-  { id: 2, message: "It's my first post", likesCount: 14 },
-];
-
-let postElements = posts.map ((m) => <Post message={m.message} likesCount={m.likesCount} /> )
-
 const MyPosts = (props) => {
+  let postElements = props.posts.map((p) => (<Post message={p.message} likesCount={p.likesCount} />));
+
   return (
     <div className={classes.myPosts}>
       <h3>my post</h3>
@@ -21,9 +16,7 @@ const MyPosts = (props) => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={classes.posts}>
-        {postElements}
-      </div>
+      <div className={classes.posts}>{postElements}</div>
     </div>
   );
 };
