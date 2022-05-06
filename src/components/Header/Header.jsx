f8988ae24667bb7e492593e50189786a5b1179e6
Vova Traihel <vovatraigel@gmail.com>
2022-05-07 01:11:24 +0300

    61 login refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from'./Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
       <img src="https://e7.pngegg.com/pngimages/65/926/png-clipart-inkscape-graphics-editor-graphics-software-corel-draw-logo-silhouette-black-thumbnail.png" />
+      <div className={classes.loginBlock}>
+        {props.isAuth ? props.login
+          :<NavLink to={'/login'}>Login</NavLink>}
+        
+      </div>
     </header>
   );
 };
